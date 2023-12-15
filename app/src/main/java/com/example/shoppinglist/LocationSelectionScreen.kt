b2f2887e9ef7a77091de16c29640a701806c6b1c
Salih Furkan <slhfurkaan@gmail.com>
2023-12-15 16:55:27 +0300

    Uploaded Files

---
+++ app/src/main/java/com/example/shoppinglist/LocationSelectionScreen.kt
@@ -0,0 +1,26 @@
+package com.example.shoppinglist
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.rememberCameraPositionState
+
+@Composable
+fun LocationSelectionScreen(
+    location: LocationData,
+    onLocationSelected : (LocationData) -> Unit
+){
+
+    val userLocation = remember {
+        mutableStateOf(LatLng(location.latitude, location.longitude))
+    }
+
+    var cameraPositionState = rememberCameraPositionState{
+        position = CameraPosition.fromLatLngZoom(userLocation.value, 10f)
+    }
+
+
+
+}
