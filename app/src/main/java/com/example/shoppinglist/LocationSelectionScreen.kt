a8709444e29f61779e2fb3e68a3cf2a09eaf3135
Salih Furkan <slhfurkaan@gmail.com>
2023-12-15 17:06:03 +0300

    Uploaded Files

--- app/src/main/java/com/example/shoppinglist/LocationSelectionScreen.kt
+++ app/src/main/java/com/example/shoppinglist/LocationSelectionScreen.kt
@@ -1,10 +1,20 @@
 package com.example.shoppinglist
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
 
 @Composable
@@ -21,6 +31,29 @@ fun LocationSelectionScreen(
         position = CameraPosition.fromLatLngZoom(userLocation.value, 10f)
     }
 
+    Column(modifier = Modifier.fillMaxSize()) {
+
+        GoogleMap(
+            modifier = Modifier
+                .weight(1f)
+                .padding(top = 16.dp),
+            cameraPositionState = cameraPositionState,
+            onMapClick = {
+                userLocation.value = it
+            }
+        ) {
+            Marker(state = MarkerState(position=userLocation.value)) // The red dot showing your location in Google maps
+        }
+
+        var newLocation : LocationData
+
+        Button(onClick = {
+            newLocation = LocationData(userLocation.value.latitude, userLocation.value.longitude)
+            onLocationSelected(newLocation)
+        }) {
+            Text("Set Location")
+        }
+    }
 
 
 }
