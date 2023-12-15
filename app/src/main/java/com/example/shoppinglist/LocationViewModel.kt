3e19f80c3864f1ce1163315c4b1da30a789f5c6d
Salih Furkan <slhfurkaan@gmail.com>
2023-12-16 00:26:33 +0300

    Uploaded Files

--- app/src/main/java/com/example/shoppinglist/LocationViewModel.kt
+++ app/src/main/java/com/example/shoppinglist/LocationViewModel.kt
@@ -1,16 +1,35 @@
 package com.example.shoppinglist
 
+import android.util.Log
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
 class LocationViewModel : ViewModel() {
 
     private val _location = mutableStateOf<LocationData?>(null)
     val location = _location
 
+    private val _address = mutableStateOf(listOf<GeocodingResult>())
+    val address = _address
+
     fun updateLocation(newLocation : LocationData){
         _location.value = newLocation
     }
 
+    fun fetchAddress(latlng : String, ){
+        try{
+            viewModelScope.launch {
+                val result = RetrofitClient.create().getAddressFromCoordinates(
+                    latlng,
+                    "paste here your api key"
+                )
+                _address.value = result.results
+            }
+        }catch (e : Exception){
+            Log.d("res1",  "${e.cause} ${e.message}")
+        }
+    }
 
 }
